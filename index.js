c96c56d6a83dd9e1137208db32bdd90ab3b00db6
razashekarkodi <45877624+razashekarkodi@users.noreply.github.com>
2021-06-10 20:34:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/',function(req,res) {
+  console.log('Running ...');
   res.send("Hello World");
 });
-
+console.log('Running ...');
 app.listen(4000);
